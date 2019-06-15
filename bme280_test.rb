c34eb31f9d23a39618da6178fc07c23f372a73ce
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2019-06-15 16:31:08 +0200

    update

--- bme280_test.rb
+++ bme280_test.rb
@@ -3,12 +3,12 @@
 require 'i2c_drivers'
 require 'i2c_drivers/device/bme280'
 
-device = I2CDrivers::Bme280.new(address: 0x77)
+device = I2CDrivers::Device::Bme280.new(address: 0x77)
 
 p device.read_id
 p device
 
-device.write_config(I2CDrivers::Bme280::T_STANDBY_0_5MS, I2CDrivers::Bme280::FILTER_16)
-device.write_ctrl_hum(I2CDrivers::Bme280::OVERSAMPLE_1)
-device.write_ctrl_meas(I2CDrivers::Bme280::OVERSAMPLE_16, I2CDrivers::Bme280::OVERSAMPLE_2, I2CDrivers::Bme280::MODE_NORMAL)
+device.write_config(I2CDrivers::Device::Bme280::T_STANDBY_0_5MS, I2CDrivers::Device::Bme280::FILTER_16)
+device.write_ctrl_hum(I2CDrivers::Device::Bme280::OVERSAMPLE_1)
+device.write_ctrl_meas(I2CDrivers::Device::Bme280::OVERSAMPLE_16, I2CDrivers::Device::Bme280::OVERSAMPLE_2, I2CDrivers::Device::Bme280::MODE_NORMAL)
 p device.calc_sensor_data
