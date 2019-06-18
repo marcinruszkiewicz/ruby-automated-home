a9afac685e13191a5c8b56c7869543c7a78e5d11
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2019-06-18 11:36:49 +0200

    test if it actually works

---
+++ rubyhome_bme280.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'pry'
+require 'ruby_home'
+require 'i2c_drivers'
+require 'i2c_drivers/device/bme280'
+
+sensor = I2CDrivers::Device::Bme280.new(address: 0x77)
+# accessory_information = RubyHome::ServiceFactory.create(:accessory_information)
+temperature_sensor = RubyHome::ServiceFactory.create(
+  :temperature_sensor,
+  current_temperature: 0.0
+)
+humidity_sensor = RubyHome::ServiceFactory.create(
+  :humidity_sensor,
+  current_relative_humidity: 0.0
+)
+
+Thread.new do
+  sleep 10
+
+  loop do
+    data = sensor.calc_sensor_data
+
+    temperature_sensor.current_temperature = data[:temperature]
+    humidity_sensor.current_relative_humidity = data[:humidity]
+
+    sleep 30
+  end
+end
+
+# binding.pry
+RubyHome.run
